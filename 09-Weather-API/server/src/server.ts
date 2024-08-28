3f100ebaa6ed332e0346b90462ab6f8747cf1503
haydencfb <haydenfitzbrintle@gmail.com>
2024-08-28 10:49:06 -0700

    last commit

--- 09-Weather-API/server/src/server.ts
+++ 09-Weather-API/server/src/server.ts
@@ -9,9 +9,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static('../client/dist'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(routes);
 
 // Start the server on the port
--- 09-Weather-API/server/src/service/historyService.ts
+++ 09-Weather-API/server/src/service/historyService.ts
@@ -21,11 +21,11 @@ class City {
 
 class HistoryService {
   private async read() {
-    return await fs.readFile("db.json", "utf-8");
+    return await fs.readFile("db/db.json", "utf-8");
   }
 
   private async write(cities: City[]) {
-    return await fs.writeFile("db.json", JSON.stringify(cities, null, 2));
+    return await fs.writeFile("db/db.json", JSON.stringify(cities, null, 2));
   }
 
   async getCities() {
@@ -56,8 +56,12 @@ class HistoryService {
   }
 
   // * BONUS TODO: Define a removeCity method that removes a city from the db.json file
-  // async removeCity(id: string) {}
-  
+  async removeCity(id: string) {
+    return await this.getCities().then(cities => {
+      const updatedCities = cities.filter(city => city.id !== id);
+      return this.write(updatedCities);
+    });
+  }
 }
 
 export default new HistoryService();
